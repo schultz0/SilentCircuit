d328d21d4893707603a496e6a415ed3472a36631
J <jimfinoc@gmail.com>
2016-04-24 17:14:25 -0400

    Identifies the System type

--- Server.py
+++ Server.py
@@ -1,26 +1,44 @@
 import serial
 import os
+import platform
+
+
+## This lets us know if we are running the program on a Mac or Linux Computer. 
+#print platform.system()
+if platform.system() == 'Darwin':
+    computerSystem = 'Mac'
+elif platform.system() == 'Darwin':
+    computerSystem = 'Linux'
+else:
+    computerSystem = 'Unknown'
+
+print computerSystem
 
 ###need the output of ls /dev/cu.usbmodem*
 #ArdunioLocation = ????
 
-#command for a Mac Computer
-retvalue = os.system("ls /dev/cu.usbmodem*")
-# command for a Raspberry Pi Computer
-retvalue = os.system("ls /dev/ttyAMC*")
-
-print retvalue
-
-
-import os
-p = os.popen('command',"r")
-while 1:
-    line = p.readline()
-    if not line: break
-    print line
+##command for a Mac Computer
+#retvalue = os.system("ls /dev/cu.usbmodem*")
+## command for a Raspberry Pi Computer
+#
+#print retvalue
+#
+#retvalue = os.system("ls /dev/ttyAMC*")
+#
+#print retvalue
 
 
+#
+#
+#import os
+#p = os.popen('command',"r")
+#while 1:
+#    line = p.readline()
+#    if not line: break
+#    print line
+#
 
-ser = serial.Serial(ArdunioLocation, 115200)
-while True:
-	print ser.readline(),
+
+#ser = serial.Serial(ArdunioLocation, 115200)
+#while True:
+#	print ser.readline(),
